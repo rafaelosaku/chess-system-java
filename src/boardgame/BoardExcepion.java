1cbeed59b623983eb88f3945f64cc57cd5917a08
Rafael Osaku <rflosaku@hotmail.com>
2020-06-19 11:53:49 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardExcepion.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+public class BoardExcepion extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExcepion(String msg) {
+		super(msg);
+	}
+
+}
